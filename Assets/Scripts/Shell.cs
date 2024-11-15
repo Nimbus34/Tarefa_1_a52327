2605f78fbafed0dcc26249cc2c7bc0f1449a3aa5
Nimbus34 <leonardo.f.silva546@gmail.com>
2024-11-15 19:15:00 +0000

    Physics Ai: Acceleration

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Shell : MonoBehaviour
 {
     public GameObject explosion;
+    float force = 2;
+    float mass = 2;
+    float drag = 1;
+    float yspeed = 0;
+    float gravity = -9.8f;
+    float graivtyAcceleration;
+    float acceleration;
+    float speed = 0;
 
     void OnCollisionEnter(Collision col)
     {
@@ -19,12 +27,16 @@ public class Shell : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        acceleration = force / mass;
+        speed = acceleration * 1;
+        graivtyAcceleration = gravity / mass;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-
+        speed *= (1 - Time.deltaTime * drag);
+        yspeed += graivtyAcceleration * Time.deltaTime;
+        this.transform.Translate(0, yspeed, speed);
     }
 }
