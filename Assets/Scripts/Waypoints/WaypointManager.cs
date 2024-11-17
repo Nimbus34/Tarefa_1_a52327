f98f78a27d1efaa200d3fe547e0e65305d201383
Nimbus34 <leonardo.f.silva546@gmail.com>
2024-11-17 17:15:00 +0000

    Waypoints: A simple Graph API

--- Assets/Scripts/Waypoints/WaypointManager.cs
+++ Assets/Scripts/Waypoints/WaypointManager.cs
@@ -16,10 +16,25 @@ public class WaypointManager : MonoBehaviour
 
     public GameObject[] waypoint;
     public link[] links;
+    public Graph graph = new Graph();
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(waypoint.Length > 0)
+        {
+            foreach(GameObject wp in waypoint)
+            {
+                graph.AddNode(wp);
+            }
+            foreach(link l in links)
+            {
+                graph.AddEdge(l.node1, l.node2);
+                if(l.dir == link.direction.BI)
+                {
+                    graph.AddEdge(l.node2, l.node1);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
